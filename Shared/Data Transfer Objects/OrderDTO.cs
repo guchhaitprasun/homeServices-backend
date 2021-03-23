4c28aa0aec36523a177a91c82c6f9d3b7622ae8a
Prasun Guchhait <prasunguchhait1997@gmail.com>
2021-03-23 12:54:40 +0530

    Added New Shared Class

---
+++ Shared/Data Transfer Objects/OrderDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class OrderDTO
+    {
+        public OrderDTO()
+        {
+            //IsActive = true;
+            //Service = new ServiceDTO();
+            //Region = new AvailabilityRegionDTO();
+            //UserDetails = new UserDTO();
+            //ServiceProviderDetails = new ServiceProvider();
+            //OrderStatus = "Service Requested";
+            //StatusID = 1; //1 for requested 
+            //              //2 for awaiting approval 
+            //              //3 for Service Request approve
+            //              //4 for service Provice successfully
+            //              //0 for service Rejected
+                            //5 for service cancelled
+        }
+        public string ReferenceNumber { get; set; }
+        public ServiceDTO Service { get; set; }
+        public AvailabilityRegionDTO Region { get; set; }
+        public UserDTO UserDetails { get; set; }
+
+        public string OrderStatus { get; set; }
+        public int StatusID { get; set; }
+        public ServiceProvider ServiceProviderDetails { get; set; }
+
+        public bool IsActive { get; set; }
+        public bool IsRejected { get; set; }
+
+    }
+}
