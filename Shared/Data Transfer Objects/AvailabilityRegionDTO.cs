3e950052d4b87af35de63218a2af1ea808e1c073
Prasun Guchhait <prasunguchhait1997@gmail.com>
2021-03-23 11:53:45 +0530

    Shared Classes

---
+++ Shared/Data Transfer Objects/AvailabilityRegionDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    /// <summary>
+    /// Schema for Availaibilty Region 
+    /// Author: Prasun Guchhait 
+    /// </summary>
+    public class AvailabilityRegionDTO
+    {
+        public int RegionID { get; set; }
+        public string RegionName { get; set; }
+        public decimal? ServiceAmount { get; set; }
+        public DateTime? NextAvailableDate { get; set; }
+    }
+}
