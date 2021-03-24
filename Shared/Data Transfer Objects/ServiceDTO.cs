77bea4a6a677e4bd71cfbfa2d13307bb54a5f10f
Prasun Guchhait <prasunguchhait1997@gmail.com>
2021-03-24 11:55:37 +0530

    Added New Shared Class

---
+++ Shared/Data Transfer Objects/ServiceDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    /// <summary>
+    /// Schema for Services
+    /// Author: Prasun Guchhait 
+    /// </summary>
+    public class ServiceDTO
+    {
+        public int ServiceID { get; set; }
+        public string ServiceName { get; set; }
+        public IList<AvailabilityRegionDTO> AvailabilityRegions { get; set; }
+    }
+}
