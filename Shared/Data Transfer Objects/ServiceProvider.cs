97fcbea91777f19971f2db0c6330b2d7f5112912
Prasun Guchhait <prasunguchhait1997@gmail.com>
2021-03-24 11:55:56 +0530

    Add New Shared Class

---
+++ Shared/Data Transfer Objects/ServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class ServiceProvider
+    {
+        public int ProviderId { get; set; }
+        public string ProviderName { get; set; }
+        public int ServiceTypeID { get; set; }
+        public string ServiceName { get; set; }
+        public DateTime EstimatedArrivaltime { get; set; }
+        public int AmountToBePaid { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
---
+++ Shared/Data Transfer Objects/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class UserDTO
+    {
+        public int UserId { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string AuthenticationToken { get; set; }
+
+        public List<OrderDTO> MyOrders { get; set; }
+
+    }
+}
